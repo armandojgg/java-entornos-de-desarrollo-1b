e3cffa724f16532739418ff99685d1309fa55964
armandojgg <162715672+armandojgg@users.noreply.github.com>
2024-03-14 14:07:22 +0100

    Add files via upload

---
+++ Persona/src/modelos/Model/Persona.java
@@ -0,0 +1,67 @@
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class Persona {
+
+    /**
+     * Default constructor
+     */
+    public Persona() {
+    }
+
+    /**
+     * 
+     */
+    private String dni;
+
+    /**
+     * 
+     */
+    private String nombre;
+
+    /**
+     * 
+     */
+    private String apellido1;
+
+    /**
+     * 
+     */
+    private String apellido2;
+
+    /**
+     * 
+     */
+    private int edad = 0;
+
+    /**
+     * @param dni 
+     * @param nombre 
+     * @param apellido1 
+     * @param apellido2 
+     * @param edad
+     */
+    public void Persona(String dni, String nombre, String apellido1, String apellido2, int edad) {
+        // TODO implement here
+    }
+
+    /**
+     * @param dni
+     */
+    public void setDni(String dni) {
+        return this.dni;
+    }
+
+    /**
+     * null
+     * @return
+     */
+    public String getDni() {
+        // TODO implement here
+        return this.dni;
+    }
+
+}
